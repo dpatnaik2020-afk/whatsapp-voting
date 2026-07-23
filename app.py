f9b5b75c5eec2e8e2789ab6041f0699237c16aeb
dpatnaik2020-afk <dpatnaik2020@gmail.com>
2026-07-23 06:58:15 -0700

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ ELECTION_TITLE = "Class / Group Voting System"
 CANDIDATES = ["ANEESH SAHU", "DARSHIL PATNAIK", "DIPAK KUMAR PRADHAN","SAHIL SAMBHAV SWAIN"]
 
 # Set your deadline year, month, day, hour, minute
-DEADLINE = datetime.datetime(2026, 7, 30, 19, 25, 7, tzinfo=ZoneInfo("Asia/Kolkata"))
+DEADLINE = datetime.datetime(2026, 7, 30, 19,59, 7, tzinfo=ZoneInfo("Asia/Kolkata"))
 
 # =======================================================
 
